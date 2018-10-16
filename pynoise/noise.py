4c69ec04d512e792926336cf2ca5ad7988701e01
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2018-10-16 16:41:23 -0300

    Initial docs

--- pynoise/noise.py
+++ pynoise/noise.py
@@ -1,41 +1,61 @@
+"""This module contains functions to work with signals and noise.
+
+.. module:: noise
+    :synopsis: Noise functions for signals.
+    
+.. moduleauthor:: Marco Guerreiro <marcotulio.guerreiro@gmail.com>
+
+"""
 import numpy as np
 
 def awgn(x, snr, out='signal'):
 
-    """Introduz ruído AWGN em um sinal.
+    """Adds White Gaussian Noise to a signal.
 
-    Args:
-        x (np.ndarray): Sinal a ser adicionado ruído.
-        snr (int, float): SNR desejado.
-        out (str): Tipo de retorno. Se 'signal', retorna o sinal com o
-            ruído. Se 'noise', retorna somente o ruído. Se 'both',
-            retorna uma lista, em que o primeiro elemento é o sinal com
-            o ruído e o segundo elemento é o ruído. Para qualquer outro
-            valor, a função retorna o sinal com o ruído.
+    The noise level is specified as a Signal-to-Noise Ratio (SNR) value.
+    The SNR is defined as:
+    
+    .. math:: SNR = 10\\log_{10}\\left(\\frac{E_x}{E_n}\\right)
 
-    Returns:
-        Corrupted signal
+    where :math:`E_x` is the signal power and :math:`E_n` is the noise
+    power. The noise of a discrete signal can be computed as:
+
+    .. math:: E = \\frac{1}{N}\sum_{k=0}^{N - 1}|x_k|^2
+    
+    Parameters
+    ----------
+        x : ``np.ndarray``
+            Signal.
+        snr : int, float
+            Signal-to-Noise ration.
+        out : str, optional
+            Output data. If 'signal', the signal `x` plus noise is
+            returned. If 'noise', only the noise vector is returned. If
+            'both', signal with noise and noise only are returned. Any
+            other value defaults to 'signal'.
+
+    Returns
+    -------
+    ``np.ndarray``
+        Corrupted signal.
+        
     """
-
     if type(x) is list:
         x = np.array(x)
     
-    #N = len(x)
     N = x.shape[0]
     
-    # First, compute signal power
-    #Ps = sum(x**2/N)
+    # Signal power
     Ps = np.sum(x**2/N, axis=0)
     
     Psdb = 10*np.log10(Ps)
 
-    # Next, we compute how much noise we need
+    # Noise level necessary
     Pn = Psdb - snr
 
-    # Finally, we compute the noise vector
+    # Noise vector
     n = np.sqrt(10**(Pn/10))*np.random.normal(0, 1, x.shape)
 
-    # If out is y, returns signal plus noise. Else, returns noise only
     if out == 'signal':
         return x + n
     elif out == 'noise':
@@ -44,4 +64,3 @@ def awgn(x, snr, out='signal'):
         return x + n, n
     else:
         return x + n
-
