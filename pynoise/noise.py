Corrected computation when signal is a matrix

pynoise/noise.py
@@ -8,7 +8,7 @@
 """
 import numpy as np
 
-def awgn(x, snr, out='signal'):
+def awgn(x, snr, out='signal', method='vectorized'):
 
     """Adds White Gaussian Noise to a signal.
 
@@ -24,8 +24,8 @@ def awgn(x, snr, out='signal'):
     
     Parameters
     ----------
-        x : ``np.ndarray``
-            Signal.
+        x : `np.ndarray`
+            Signal, as a vector or column-matrix.
         snr : int, float
             Signal-to-Noise ration.
         out : str, optional
@@ -33,27 +33,42 @@ def awgn(x, snr, out='signal'):
             returned. If 'noise', only the noise vector is returned. If
             'both', signal with noise and noise only are returned. Any
             other value defaults to 'signal'.
+        method: str, optional
+            Method to compute noise vector (matrix) to be introduced in the
+            signal. In the 'vectorized' method, the matrix energy is computed
+            and used to compute the noise energy. In the 'max_en' method, the
+            energy of each column of `x` is computed and only the highest
+            value is used to compute the noise energy. The 'vectorized' method
+            is used by default.
 
     Returns
     -------
-    ``np.ndarray``
+    `np.ndarray`
         Corrupted signal.
-        
-    """
-    if type(x) is list:
-        x = np.array(x)
-    
+
+    Raises
+    ------
+    ValueErrorExpection
+        If `method` is not recognized.
+
+    """    
     N = x.shape[0]
     
     # Signal power
-    Ps = np.sum(x**2/N, axis=0)
+    # Ps = np.sum(x**2/N, axis=0)
+    if method == 'vectorized':
+        Ps = np.sum(x**2/N)
+    elif method == 'max_en':
+        Ps = np.max(np.sum(x**2/N, axis=0))
+    else:
+        raise ValueError('method \"' + str(method) + '\" not recognized.')
     
     Psdb = 10*np.log10(Ps)
 
     # Noise level necessary
     Pn = Psdb - snr
 
-    # Noise vector
+    # Noise vector (or matrix)
     n = np.sqrt(10**(Pn/10))*np.random.normal(0, 1, x.shape)
 
     if out == 'signal':