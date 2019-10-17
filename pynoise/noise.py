1ed2545e64af4db8f4419868a9c1aa75a839ffad
Marco Guerreiro <marcotulio.guerreiro@gmail.com>
2019-10-17 16:41:03 -0300

    Option to compute noise for each column of a matrix

--- pynoise/noise.py
+++ pynoise/noise.py
@@ -12,8 +12,8 @@ This module contains functions to work with signals and noise.
 """
 import numpy as np
 
-def awgn(x, snr, out='signal', method='vectorized', axis=0):
 
+def awgn(x, snr, out='signal', method='vectorized', axis=0):
     r"""Adds White Gaussian Noise to a signal.
 
     The noise level is specified as a Signal-to-Noise Ratio (SNR) value,
@@ -32,22 +32,27 @@ def awgn(x, snr, out='signal', method='vectorized', axis=0):
     ----------
     x : `np.ndarray`
         Signal, as a vector or column-matrix.
+        
     snr : int, float
         Signal-to-Noise ration.
+        
     out : str, optional
         Output data. If 'signal', the signal `x` plus noise is returned. If
         'noise', only the noise vector is returned. If 'both', signal with
         noise and noise only are returned. Any other value defaults to 'signal'.
+        
     method : str, optional
         Method to compute noise vector (matrix) to be introduced in the
         signal. In the 'vectorized' method, the matrix energy is computed and
         used to compute the noise energy. In the 'max_en' method, the energy
         of each column of `x` is computed and only the highest value is used
-        to compute the noise energy. The 'vectorized' method is used by
+        to compute the noise energy. In the 'axial' method, each column of the
+        array is considered as a signal. The 'vectorized' method is used by
         default.
+        
     axis : int, optional
-        When ``method`` is 'max_en', informs which axis to consider as signal.
-        The dafault is 0.
+        When ``method`` is 'max_en' or 'axis', informs which axis to consider
+        as signal. The dafault is 0.
 
     Returns
     -------
@@ -72,20 +77,27 @@ def awgn(x, snr, out='signal', method='vectorized', axis=0):
     # Signal power
     if method == 'vectorized':
         N = x.size
-        Ps = np.sum(x**2/N)
+        Ps = np.sum(x ** 2 / N)
+
     elif method == 'max_en':
         N = x.shape[axis]
-        Ps = np.max(np.sum(x**2/N, axis=axis))
+        Ps = np.max(np.sum(x ** 2 / N, axis=axis))
+
+    elif method == 'axial':
+        N = x.shape[axis]
+        Ps = np.sum(x ** 2 / N, axis=axis)
+        
     else:
         raise ValueError('method \"' + str(method) + '\" not recognized.')
-    
-    Psdb = 10*np.log10(Ps)
+
+    # Signal power, in dB
+    Psdb = 10 * np.log10(Ps)
 
     # Noise level necessary
     Pn = Psdb - snr
 
     # Noise vector (or matrix)
-    n = np.sqrt(10**(Pn/10))*np.random.normal(0, 1, x.shape)
+    n = np.sqrt(10 ** (Pn / 10)) * np.random.normal(0, 1, x.shape)
 
     if out == 'signal':
         return x + n
